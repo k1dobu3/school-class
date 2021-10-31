14c7fe16a32b68415c8e174737b4c2d04853bb5a
Kirill <91422323+k1dobu3@users.noreply.github.com>
2021-10-31 18:10:36 +0300

    Add files via upload

---
+++ DniNedeli/DniNedeli/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DniNedeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер вашего класса:");
+            double x = Convert.ToInt32(Console.ReadLine());
+            switch (x)
+            {
+                case 0:
+                    Console.WriteLine("Ребенок, отойди от компьютера!");
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine("Вы в младшем классе!");
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    Console.WriteLine("Вы в среднем классе!");
+                    break;
+                case 10:
+                case 11:
+                    Console.WriteLine("Вы в старшем классе!");
+                    break;
+                default:
+                    Console.WriteLine("Вы в рабочем классе!");
+                    break;
+            }
+        }
+    }
+}
